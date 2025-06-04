b9687fac2b8bc39bbed7431dacc6822f4980c062
preeti <preetir@iiti.ac.in>
2025-06-04 12:19:49 +0530

    Added start script for Render deployment

---
+++ src/routes/contactRoutes.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const contactController_1 = require("../controllers/contactController");
+const router = express_1.default.Router();
+router.get('/', contactController_1.getContacts);
+router.post('/', contactController_1.createContact);
+router.put('/:id', contactController_1.updateContact);
+router.delete('/:id', contactController_1.deleteContact);
+exports.default = router;
